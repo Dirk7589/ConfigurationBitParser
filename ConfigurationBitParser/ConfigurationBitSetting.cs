b1cd98269a1d78c69cec61472f88ebef7eaffa6f
Dirk Dubois <dirk.dubois@mail.mcgill.ca>
2013-10-14 16:13:36 -0400

    Initial commit

---
+++ ConfigurationBitParser/ConfigurationBitSetting.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace ConfigurationBitSetting
+{
+    
+    class ChipInfoReader
+    {
+        string htmlDirectoryPath = null;
+        List<ConfigBits> bits;
+
+        public ChipInfoReader(string htmlDirectoryPath, List<ConfigBits> bits)
+        {
+            this.htmlDirectoryPath = htmlDirectoryPath;
+            this.bits = bits;
+        }
+
+        public List<ConfigBits> getBits()
+        {
+            return bits;
+        }
+
+        public void Read(string htmlPath)
+        {
+            int numberOfConfigBits = 0;
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.Load(htmlPath);
+            if (doc.DocumentNode != null)
+            {
+                HtmlNodeCollection tableNodes = doc.DocumentNode.SelectNodes("//table");
+                numberOfConfigBits = tableNodes.Count();
+                //Console.WriteLine(numberOfConfigBits.ToString());
+                int count = 1;
+                foreach (HtmlNode table in tableNodes)
+                {
+                    ConfigBits bit = new ConfigBits();
+                    //Select each configuration bit
+                    HtmlNodeCollection tableBody = table.ChildNodes;
+
+                    HtmlNode configBitNameNode = table.SelectSingleNode("/html[1]/body[1]/table[" + count.ToString() + "]/tr[1]/td[1]");
+                    HtmlNode configBitDescriptionNode = table.SelectSingleNode("/html[1]/body[1]/table[" + count.ToString() + "]/tr[1]/td[2]");
+
+                    bit.setConfigBitName(configBitNameNode.InnerText);
+                    bit.setConfigBitDescription(configBitDescriptionNode.InnerText);
+
+
+                    HtmlNodeCollection configValueNodes = table.SelectNodes("/html[1]/body[1]/table[" + count.ToString() + "]/tr[position()>1]/td[1]");
+                    HtmlNodeCollection configDescriptionNodes = table.SelectNodes("/html[1]/body[1]/table[" + count.ToString() + "]/tr[position()>1]/td[2]");
+                    foreach (HtmlNode value in configValueNodes)
+                    {
+                        bit.addConfigSetting(value.InnerText);
+                    }
+                    foreach (HtmlNode description in configDescriptionNodes)
+                    {
+                        bit.addConfigDescription(description.InnerText);
+                    }
+
+                    bits.Add(bit);
+                    count++;
+                }
+            }
+        }
+    }
+    class ConfigBits
+    {
+        string configBitName;
+        string configBitDescription;
+        List<string> configSetting;
+        List<string> configDescription;
+
+        public ConfigBits()
+        {
+            this.configSetting = new List<string>();
+            this.configDescription = new List<string>();
+        }
+
+        public void setConfigBitName(string configBitName)
+        {
+            this.configBitName = configBitName;
+        }
+
+        public void setConfigBitDescription(string configBitDescription)
+        {
+            this.configBitDescription = configBitDescription;
+        }
+
+        public void setConfigSetting(List<string> configSetting)
+        {
+            this.configSetting = configSetting;
+        }
+
+        public void setConfigDescription(List<string> configDescription)
+        {
+            this.configDescription = configDescription;
+        }
+
+        public void addConfigInfo(string configValue, string configDescription)
+        {
+            this.configSetting.Add(configValue);
+            this.configDescription.Add(configDescription);
+        }
+
+        public void addConfigSetting(string configSetting)
+        {
+            this.configSetting.Add(configSetting);
+        }
+
+        public void addConfigDescription(string configDescription)
+        {
+            this.configDescription.Add(configDescription);
+        }
+
+        public int getConfigSettingCount()
+        {
+            return this.configSetting.Count;
+        }
+
+        public int getConfigDescriptionCount()
+        {
+            return this.configDescription.Count;
+        }
+
+        public string getConfigBitName()
+        {
+            return configBitName;
+        }
+
+        public string getConfigBitDescription()
+        {
+            return configBitDescription;
+        }
+
+        public List<string> getConfigSetting()
+        {
+            return this.configSetting;
+        }
+        public string getConfigSetting(int index)
+        {
+            return this.configSetting[index];
+        }
+
+        public List<string> getConfigDescription()
+        {
+            return this.configDescription;
+        }
+        public string getConfigDescription(int index)
+        {
+            return this.configDescription[index];
+        }
+
+    }
+    
+}
---
+++ ConfigurationBitParser/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ConfigurationBitSetting;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace ConfigurationBitParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string htmlDirectoryPath = @"C:\Program Files (x86)\Microchip\xc8\v1.12\docs\chips";
+        string htmlFilePath = null;
+        List<string> htmlFiles = new List<string>();
+        ChipInfoReader reader;
+        List<ConfigBits> bits;
+        ObservableCollection<string> listOfChips = new ObservableCollection<string>();
+        ObservableCollection<string> listOfConfigBits = new ObservableCollection<string>();
+        ObservableCollection<string> listOfConfigValues = new ObservableCollection<string>();
+        FolderBrowserDialog folderBrowse;
+
+        BackgroundWorker bw = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            folderBrowse = new FolderBrowserDialog();
+            this.selectChip.ItemsSource = listOfChips;
+            this.configBits.ItemsSource = listOfConfigBits;
+            this.configValues.ItemsSource = listOfConfigValues;
+            bw.DoWork +=new DoWorkEventHandler(bw_DoWork);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            try
+            {
+                getChips(htmlDirectoryPath);
+            }
+            catch (Exception e)
+            {
+                bool correctPath = false;
+                while (!correctPath)
+                {
+                    System.Windows.MessageBox.Show("Unable to find default doc path" +e.Message.ToString(), "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    if (folderBrowse.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        if (folderBrowse.SelectedPath.Contains("chips"))
+                        {
+                            htmlDirectoryPath = folderBrowse.SelectedPath;
+                            getChips(htmlDirectoryPath);
+                            correctPath = true;
+                        }
+                        else
+                        {
+                            System.Windows.MessageBox.Show("You have selected the incorrect path, must end with docs", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                    }
+                    else if (folderBrowse.ShowDialog() == System.Windows.Forms.DialogResult.Cancel || folderBrowse.ShowDialog() == System.Windows.Forms.DialogResult.Abort)
+                    {
+                        System.Windows.Application.Current.Shutdown();
+                    }
+                }
+            }
+        }
+
+        private void bw_DoWork(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+        public void getChips(string htmlDirectoryPath)
+        {
+            foreach (string file in Directory.GetFiles(htmlDirectoryPath))
+            {
+                htmlFiles.Add(file);
+                listOfChips.Add(System.IO.Path.GetFileNameWithoutExtension(file));
+            }
+
+        }
+
+        private void selectChip_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            outputText.Clear();
+            listOfConfigBits.Clear();
+            listOfConfigValues.Clear();
+            htmlFilePath = selectChip.SelectedItem.ToString();
+            bits = new List<ConfigBits>();
+            reader = new ChipInfoReader(htmlDirectoryPath, bits);
+
+            this.listOfConfigBits.Clear();
+            htmlFilePath = htmlFiles[selectChip.SelectedIndex];
+            reader.Read(htmlFilePath);
+            foreach (ConfigBits bit in bits)
+            {
+                this.listOfConfigBits.Add(bit.getConfigBitName() + " " + bit.getConfigBitDescription());
+            }
+        }
+
+        private void configBits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listOfConfigValues.Clear();
+            int bitSelected = configBits.SelectedIndex;
+            for (int i = 0; i < bits[bitSelected].getConfigSettingCount(); i++)
+            {
+                listOfConfigValues.Add(bits[bitSelected].getConfigSetting(i) + " " + bits[bitSelected].getConfigDescription(i));
+            }
+        }
+
+        private void setBit_Click(object sender, RoutedEventArgs e)
+        {
+            if (configValues.SelectedItem != null)
+            {
+                string pragmaStatement = "#pragma config " + bits[configBits.SelectedIndex].getConfigBitName() + bits[configBits.SelectedIndex].getConfigSetting(configValues.SelectedIndex) + ", ";
+                outputText.AppendText(pragmaStatement);
+            }
+        }
+
+        private void configValues_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+         
+        }
+
+        private void clearOutput_Click(object sender, RoutedEventArgs e)
+        {
+            outputText.Clear();
+        }
+
+        /// <summary>
+        /// An event handler that opens the selected chip document in the default browser.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void openDoc_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectChip.SelectedItem != null)
+            {
+
+            }
+        }
+    }
+}
